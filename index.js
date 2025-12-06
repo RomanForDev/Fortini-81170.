4fa15f2a788217f18c395cbe6a4d9a3eb9d95829
RomanForDev <romanfortinidev@gmail.com>
2025-12-06 10:26:57 -0300

    servidor levantado en 8080

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-import http from 'http';
+// import http from 'http';
 
-const server = http.createServer((request, response) => {
-    response.end("Hola Mundo!!");
-})
+// const server = http.createServer((request, response) => {
+//     response.end("Hola Mundo!!!!");
+// })
 
+// const port = 8080;
+
+// server.listen(port, () => {
+//     console.log("Servidor activo : " + port);
+// })
+
+import express from "express";
+
+const app = express();
 const port = 8080;
 
-server.listen(port, () => {
-    console.log("Servidor activo : " + port);
+
+app.get("/", (request, response) => {
+    response.send("Hola mundo!!");
+})
+
+app.listen(port, () => {
+    console.log("Servidor activo!" + port);
 })
