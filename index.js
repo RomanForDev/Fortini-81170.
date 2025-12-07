5cc6e9bb71194eefde95f8d76f3052ee1b7c2c35
RomanForDev <romanfortinidev@gmail.com>
2025-12-07 16:10:14 -0300

    Muestra de productos desde JSON funcionando

--- index.js
+++ index.js
@@ -11,13 +11,49 @@
 // })
 
 import express from "express";
+import fs from "fs";
 
 const app = express();
 const port = 8080;
+// const productos = [{id: 1,
+//     name: "Coca",
+//     price: 3500,
+//     avalaible: true,
+// }, {id: 1,
+//     name: "Sprite",
+//     price: 3000,
+//     avalaible: false,
+// }, {id: 1,
+//     name: "Pepsi",
+//     price: 3200,
+//     avalaible: true,
+// }, {id: 1,
+//     name: "Fanta",
+//     price: 2900,
+//     avalaible: true,
+// }]; //Sólo para local sin JSON.
 
+const data = "productos.json";
+
+app.get("/productos", (request, response) => {
+    fs.readFile(data, (err, contenido) => {
+        const productos = JSON.parse(contenido)
+        response.send(productos);
+    })
+})
 
 app.get("/", (request, response) => {
-    response.send("Hola mundo!!");
+    response.send("Bienvenidos!!");
+})
+
+app.get("/productos/:prodId", (request, response) => {
+    // const data = "productos.json";
+    const id = request.params.prodId;
+    fs.readFile(data, "utf-8", (err, contenido) => {
+        let productos = JSON.parse(contenido);
+        const producto = productos.find(item => item.id == id);
+        return response.send(producto);
+    });
 })
 
 app.listen(port, () => {
